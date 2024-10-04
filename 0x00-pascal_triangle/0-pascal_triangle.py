3fa0b14e7f22b5f989efcedde96c87b8ffaadd0e
“Sylvanus <sylvanusuzor@gmail.com>
2024-10-04 00:24:41 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
-
 def pascal_triangle(n):
     """
-    pascal triangle implementation
-    args:
-         n: number of rows in the pascal triangle
-    return:
-         a list of pascal triangle based on the rows given
+    Pascal's triangle implementation
+    Args:
+        n: number of rows in the Pascal's triangle
+    Return:
+        A list of lists representing Pascal's triangle
     """
     triangle = []
 
     for i in range(n):
-        row = [1]  # Start the row with 1's
-
-        # Calculate the inner elements (skipping the first and last)
-        if i > 1:
+        row = [1]  # Start every row with 1
+        
+        # Calculate the inner values if the row has more than 1 element
+        if i > 0:
             for j in range(1, i):
                 value = triangle[i - 1][j - 1] + triangle[i - 1][j]
                 row.append(value)
-
+        
+        # End every row with 1 if it's not the first row
         if i > 0:
             row.append(1)
-
+        
         triangle.append(row)  # Append the current row to the triangle
 
     return triangle
