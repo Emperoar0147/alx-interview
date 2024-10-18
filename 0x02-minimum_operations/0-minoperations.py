cf1822d61abe28839428ae4cc0d8c6a06de1e94c
“Sylvanus <sylvanusuzor@gmail.com>
2024-10-18 00:19:53 +0000

    alx interview

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+"""
+Module 0-minoperations
+This module defines the `minOperations` function, which calculates the
+minimum number of operations required to achieve exactly n characters
+using only "Copy All" and "Paste" operations.
+"""
+
 
 def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters.
+
+    Args:
+        n (int): The number of H characters to achieve.
+
+    Returns:
+        int: The minimum number of operations or 0 if it's not possible.
+    """
     if n <= 1:
         return 0
 
     operations = 0
-    factor = 2
+    divisor = 2
 
     while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
     return operations
-
-if __name__ == "__main__":
-    n = 4
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
