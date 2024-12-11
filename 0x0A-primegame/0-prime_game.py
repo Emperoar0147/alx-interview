cb27cbe8e49af74f2e49a74bd820d4de3998cc5e
“Sylvanus <sylvanusuzor@gmail.com>
2024-12-11 11:58:07 +0000

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,60 @@
 #!/usr/bin/python3
-"""
-0-island_perimeter
-"""
 
-
-def island_perimeter(grid):
+def isWinner(x, nums):
     """
-    Function that calculates the perimeter of the island in the grid.
+    Determine the winner of each game based on the rules.
 
     Args:
-        grid (list of list of int): 2D list representing the grid with
-        0 as water and 1 as land.
+        x (int): Number of rounds.
+        nums (list): Array of n values for each round.
 
     Returns:
-        int: The perimeter of the island.
+        str: Name of the player with the most wins ("Maria" or "Ben"), or None if tied.
     """
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
+    if not nums or x < 1:
+        return None
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                # Check top
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                # Check bottom
-                if i == rows - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                # Check left
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                # Check right
-                if j == cols - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
+    # Precompute primes up to the maximum number in nums
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
 
-    return perimeter
+    # Compute the cumulative count of primes up to each number
+    prime_counts = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_counts[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
+def sieve_of_eratosthenes(n):
+    """
+    Generate a list of primes up to n using the Sieve of Eratosthenes.
+
+    Args:
+        n (int): The maximum number to check for primality.
+
+    Returns:
+        list: A list where index i is True if i is a prime number, otherwise False.
+    """
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    return is_prime
