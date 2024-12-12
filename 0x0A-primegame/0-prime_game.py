7500fc23d5c07e5fbde982bc344a91913e226d93
“Sylvanus <sylvanusuzor@gmail.com>
2024-12-12 16:18:20 +0000

    0x0A. Prime Game0x0A. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,60 +1,77 @@
 #!/usr/bin/python3
+""" This gets island perimeter """
 
-def isWinner(x, nums):
-    """
-    Determine the winner of each game based on the rules.
 
-    Args:
-        x (int): Number of rounds.
-        nums (list): Array of n values for each round.
+def island_perimeter(grid):
+    """ Islands perimeter"""
+    sum = 0
+    for i in range(len(grid)):
+        for x in range(len(grid[i])):
+            if grid[i][x] == 1 and (i < (len(grid) - 1) and i > 0):
+                fb_ver2 = (grid[i + 1][x] != 1 or grid[i - 1][x] != 1)
+                fb_ver_n = (grid[i + 1][x] != 1 and grid[i - 1][x] != 1)
+                fb_ver = (grid[i + 1][x] == 1 and grid[i - 1][x] == 1)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+            elif grid[i][x] == 1 and (i == (len(grid) - 1)):
+                fb_ver2 = (grid[i - 1][x] != 1 or True)
+                fb_ver_n = (grid[i - 1][x] != 1 and True)
+                fb_ver = (grid[i - 1][x] == 1 and False)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+            elif grid[i][x] == 1 and (i == 0):
+                fb_ver2 = (True or grid[i + 1][x] != 1)
+                fb_ver_n = (True and grid[i + 1][x] != 1)
+                fb_ver = (False and grid[i + 1][x] == 1)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+    return sum
 
-    Returns:
-        str: Name of the player with the most wins ("Maria" or "Ben"), or None if tied.
-    """
-    if not nums or x < 1:
-        return None
 
-    # Precompute primes up to the maximum number in nums
-    max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
-
-    # Compute the cumulative count of primes up to each number
-    prime_counts = [0] * (max_num + 1)
-    for i in range(1, max_num + 1):
-        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if prime_counts[n] % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
-
-def sieve_of_eratosthenes(n):
-    """
-    Generate a list of primes up to n using the Sieve of Eratosthenes.
-
-    Args:
-        n (int): The maximum number to check for primality.
-
-    Returns:
-        list: A list where index i is True if i is a prime number, otherwise False.
-    """
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
-
-    for i in range(2, int(n ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = False
-
-    return is_prime
+def total(grid, i, x, fb_ver, fb_ver2, fb_ver_n):
+    """ to get total perimeter """
+    sum = 0
+    if x == 0 and grid[i][x] == 1:
+        if fb_ver2 and (not fb_ver_n) and grid[i][x + 1] != 1:
+            sum += 3
+        elif fb_ver_n and grid[i][x + 1] != 1:
+            sum += 4
+        elif fb_ver_n and grid[i][x + 1] == 1:
+            sum += 3
+        elif fb_ver2 and (not fb_ver_n) and grid[i][x + 1] == 1:
+            sum += 2
+        elif fb_ver and grid[i][x + 1] != 1:
+            sum += 2
+        elif fb_ver and grid[i][x + 1] == 1:
+            sum += 1
+    elif (x != len(grid[i]) - 1) and grid[i][x] == 1:
+        fb_hor = (grid[i][x + 1] == 1 and grid[i][x - 1] == 1)
+        fb_hor_n = (grid[i][x + 1] != 1 and grid[i][x - 1] != 1)
+        fb_hor2 = (grid[i][x + 1] != 1 or grid[i][x - 1] != 1)
+        if (fb_hor2 and (not fb_hor_n)) and (fb_ver2 and (not fb_ver_n)):
+            sum += 2
+        elif (fb_hor2 and (not fb_hor_n)) and fb_ver_n:
+            sum += 3
+        elif (fb_hor2 and (not fb_hor_n)) and fb_ver:
+            sum += 1
+        elif fb_hor and fb_ver_n:
+            sum += 2
+        elif fb_hor and (fb_ver2 and (not fb_ver_n)):
+            sum += 1
+        elif fb_hor_n and fb_ver_n:
+            sum += 4
+        elif fb_hor_n and fb_ver:
+            sum += 2
+        elif fb_hor_n and (fb_ver2 and (not fb_ver_n)):
+            sum += 3
+    elif grid[i][x] == 1:
+        if fb_ver2 and (not fb_ver_n) and grid[i][x - 1] != 1:
+            sum += 3
+        elif fb_ver2 and (not fb_ver_n) and grid[i][x - 1] == 1:
+            sum += 2
+        elif fb_ver_n and grid[i][x - 1] != 1:
+            sum += 4
+        elif fb_ver_n and grid[i][x - 1] == 1:
+            sum += 3
+        elif fb_ver and grid[i][x - 1] != 1:
+            sum += 2
+        elif fb_ver and grid[i][x - 1] == 1:
+            sum += 1
+    return sum
